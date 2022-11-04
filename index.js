53798df5ad54e74f180160e43b6fd6806e8e21fd
sanyaswb <sanyaswb@gmail.com>
2022-11-04 14:13:45 +0200

    completed 10_rent-price

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function rentPrice(days) {
-	// write your code here
+
+	if (days > 3 && days <= 7) {
+		return days * 25 - (days - 3) * 5;
+	} else if (days > 7) {
+		return days * 25 - ((days - 7) * 10 + 20);
+	} else {
+		return days * 25;
+	}
 }
 
 module.exports = rentPrice;
